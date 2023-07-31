de8b7e38f7b6036c8ecbb02edb58f9433e9b0319
Paul Otieno <vitalispaul48@live.com>
2023-07-31 23:18:24 +0300

    Create a generic data fetching hook

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import useGenres from "../hooks/useGenres";
+import useData from "../hooks/useData";
+import useGenres, { Genre } from "../hooks/useGenres";
 
 const GenreList = () => {
-  const { genres } = useGenres();
+  const { data } = useGenres();
   return (
     <ul>
-      {genres.map((genre) => (
+      {data.map((genre) => (
         <li key={genre.id}>{genre.name}</li>
       ))}
     </ul>
