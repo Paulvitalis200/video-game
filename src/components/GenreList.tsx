698b23112d5f547e3c3ebaeb0fc6d15c4a866b07
Paul Otieno <vitalispaul48@live.com>
2023-08-02 04:13:12 +0300

    Display genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,15 +1,30 @@
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import React from "react";
 import useData from "../hooks/useData";
 import useGenres, { Genre } from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, isLoading, error } = useGenres();
+
+  if (error) return null;
+
+  if (isLoading) return <Spinner />;
   return (
-    <ul>
+    <List>
       {data.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
+        <ListItem key={genre.id} paddingY="5px">
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+            />
+            <Text fontSize={"lg"}>{genre.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
