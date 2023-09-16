2fed4fbf15d50588f518b085d6c02ed0a9ecdcef
Paul Otieno <vitalispaul48@live.com>
2023-09-16 11:59:33 +0300

    Remove unused Platform interface

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,12 +1,8 @@
 import { GameQuery } from "../App";
 import apiClient, { FetchResponse } from "../services/api-client";
 import { useQuery } from "@tanstack/react-query";
+import { Platform } from "./usePlatforms";
 
-export interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
 export interface Game {
   id: number;
   name: string;
