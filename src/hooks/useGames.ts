6b3f37aaa077ce10c2bc6b5ebfc7b7d5c311d091
Paul Otieno <vitalispaul48@live.com>
2023-09-16 13:39:57 +0300

    Refactor: Simplify game query

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -18,8 +18,8 @@ const useGames = (gameQuery: GameQuery) => {
   const fetchGames = ({ pageParam = 1 }) =>
     apiClient.getAll({
       params: {
-        genres: gameQuery.genre?.id,
-        parent_platforms: gameQuery.platform?.id,
+        genres: gameQuery.genreId,
+        parent_platforms: gameQuery.platformId,
         ordering: gameQuery.sortOrder,
         search: gameQuery.searchText,
         page: pageParam,
